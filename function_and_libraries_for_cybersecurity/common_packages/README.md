6300b24389a0eb7b08fb7b5b8809bd273d4a753d
Or Hasson <hassonor@gmail.com>
2022-11-01 18:09:26 +0200

    adding sys and os  demos

---
+++ function_and_libraries_for_cybersecurity/common_packages/README.md
@@ -0,0 +1,3 @@
+## OS
+
+https://docs.python.org/3/library/os.html
---
+++ function_and_libraries_for_cybersecurity/common_packages/os-demo.py
@@ -0,0 +1,22 @@
+import sys
+import os
+
+print("Current Working Dir: ", os.getcwd())
+print("PID:", os.getpid())
+print("Name:", os.name)
+print("Platform: ", sys.platform)
+
+os.system("ping 127.0.0.1")
+# os.system("ping " + sys.argv[1])
+
+with open(os.path.join(sys.path[0], "log.txt"), "r") as f:
+    sys.stdout.write(f.read())
+
+# os.remove("temp.txt")
+
+
+for root, dirs, files in os.walk(".", topdown=False):
+    for name in files:
+        print(os.path.join(root, name))
+    for name in dirs:
+        print(os.path.join(root, name))
